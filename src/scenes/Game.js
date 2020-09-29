d8e2de2a43fe9a3461a4ef3c52e7752b79b7b3cd
Daniel Whiffing <danielwhiffing@gmail.com>
2020-09-29 00:11:41 -0400

    Adds drag controls

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -10,6 +10,12 @@ const SEGMENTS = [
 ]
 const frags = 360 / SEGMENTS.length / 180
 
+// TODO: win screen
+// TODO: particles
+// TODO: drop shadows
+// TODO: sound
+// TODO: backend integration
+
 export default class extends Phaser.Scene {
   constructor() {
     super({ key: 'Game' })
@@ -25,24 +31,81 @@ export default class extends Phaser.Scene {
     this.drawCursor()
     this.drawUI()
 
-    this.input.on('pointerdown', this.spin, this)
+    this.input.on('pointermove', this.move, this)
+    this.input.on('pointerdown', this.click, this)
+    this.input.on('pointerup', this.release, this)
   }
 
-  spin() {
-    this.tweens.add({
-      targets: [this.cursor],
-      scale: 1.03,
-      repeat: 22,
-      yoyo: true,
-      duration: 100,
-      ease: 'Cubic.easeOut',
-    })
+  click(pointer) {
+    if (this.spinning) return
+
+    this.isDown = true
+    this.startX = pointer.x
+    this.startY = pointer.y
+    this.startAngle = this.container.angle
+    this.startTime = +new Date()
+  }
+
+  move(pointer) {
+    if (this.spinning) return
+
+    if (this.isDown) {
+      const diff = (pointer.x - this.startX) / 5
+      this.container.angle = this.startAngle + diff
+    }
+  }
+
+  release(pointer) {
+    if (this.spinning) return
+
+    this.isDown = false
+    const timeDelta = Math.abs(this.startTime - new Date())
+    const xDelta = Math.abs(this.startX - pointer.x)
+    const force = xDelta / timeDelta
+    if (force > 3) {
+      this.spin()
+    } else {
+      this.spin(force * 100, 1000)
+    }
+  }
+
+  spin(amount = 5000 + Math.random() * 1000, duration = 8000) {
+    if (this.spinning) return
+
     this.tweens.add({
       targets: [this.container],
-      angle: 5000 + Math.random() * 1000,
-      duration: 5000,
+      angle: this.container.angle + amount,
+      duration: duration,
       ease: 'Cubic.easeOut',
+      onComplete: () => {
+        this.spinning = false
+        this.headingText.text = 'Try your luck!'
+        this.tweens.add({
+          targets: this.buttonGraphics,
+          alpha: 1,
+          duration: 500,
+        })
+      },
     })
+
+    if (amount > 1000) {
+      this.spinning = true
+      this.headingText.text = '👀'
+
+      this.tweens.add({
+        targets: this.buttonGraphics,
+        alpha: 0,
+        duration: 500,
+      })
+      this.tweens.add({
+        targets: [this.cursor],
+        scale: 1.03,
+        repeat: 22,
+        yoyo: true,
+        duration: duration / 50,
+        ease: 'Cubic.easeOut',
+      })
+    }
   }
 
   drawWheel() {
@@ -50,6 +113,7 @@ export default class extends Phaser.Scene {
 
     // Draw bg
     this.wheel.arc(0, 0, w / 2.5, 0, 6.1)
+    this.wheel.fillStyle(0x000000)
     this.wheel.fill()
     let children = []
     SEGMENTS.forEach(({ icon, label }, i) => {
@@ -110,15 +174,25 @@ export default class extends Phaser.Scene {
     const headingStyle = { ...font, fontSize: 70 }
     const bodyStyle = { ...font, fontSize: 35 }
 
-    this.add.text(w / 2, h / 7, 'Try your luck!', headingStyle).setOrigin(0.5)
-
-    this.add
-      .text(w / 2, h / 5.4, 'You have 1 free spin', bodyStyle)
+    this.headingText = this.add
+      .text(w / 2, h / 7, 'Try your luck!', headingStyle)
       .setOrigin(0.5)
+
+    // this.add
+    //   .text(w / 2, h / 5.4, 'You have 1 free spin', bodyStyle)
+    //   .setOrigin(0.5)
 
     this.buttonGraphics = this.add.graphics()
     this.buttonGraphics.fillStyle(0x000000)
     this.buttonGraphics.fillRoundedRect(w / 2 - 250, h * 0.8, 500, 120, 60)
+
+    this.buttonGraphics.setInteractive(
+      new Phaser.Geom.Rectangle(w / 2 - 250, h * 0.8, 500, 120),
+      Phaser.Geom.Rectangle.Contains,
+    )
+    this.buttonGraphics.on('pointerdown', () => {
+      this.spin()
+    })
 
     this.add
       .text(w / 2, h / 1.2, 'Spin now!', { ...bodyStyle, color: '#ffffff' })
